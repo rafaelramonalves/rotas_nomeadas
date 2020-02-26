c61fa8194f5d74335f5607a5bd600314e3bff8dd
Rafael Ramon Alves T de Oliveira <roliveira@Cedraz.local>
2020-02-26 17:03:27 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:rotas_nomeadas/TelaSecundaria.dart';
+
+void main(){
+  runApp(MaterialApp(
+    initialRoute: "/",
+    routes: {
+      "/secundaria" : (context) => TelaSecundaria(),
+    },
+    home: TelaPrincipal(),
+  ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Ir para a segunda tela"),
+              padding: EdgeInsets.all(15),
+              onPressed: (){
+                Navigator.pushNamed(context, "/secundaria");
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
